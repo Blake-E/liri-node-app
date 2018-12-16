d6e39d9a3fed10d4b1007b7c8f8d5da390029300
Blake-E <mbe083@gmail.com>
2018-12-15 20:28:41 -0600

    I uploaded the liri bot homework

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # liri-node-app
+Overview:
+LIRI is an app like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+Functionality:
+LIRI will search OMDB for movies, Bands in town for concerts, and Spotify for songs.
+
+Usage:
+run with node.js: node liri <arg1> <arg2>
+
+<arg1> can be:
+
+movie-this:
+
+searches for the <arg2> in OMDB API and renders the following information about each event to the terminal:
+
+Title of the movie.
+Year the movie came out.
+IMDB Rating of the movie.
+Rotten Tomatoes Rating of the movie.
+Country where the movie was produced.
+Language of the movie.
+Plot of the movie.
+Actors in the movie.
+and If there is no <arg2> then program will default to the movie 'Mr. Nobody.'
+
+concert-this:
+
+searches for the <arg2> in Bands in Town Artist Events API and renders the following information about each event to the terminal:
+
+Name of the venue
+Venue location
+Date of the Event (uses moment to format date as "MM/DD/YYYY")
+
+spotify-this-song:
+
+searches for the <arg2> in Bands in Town Artist Events API and renders the following information about each event to the terminal:
+
+Artist(s)
+The song's name
+A preview link of the song from Spotify
+The album that the song is from
+and If there is no <arg2> then program will default to "The Sign" by Ace of Base.
+
+do-what-it-says:
+
+reads the random.txt and uses first string as <arg1> and second string (separated by ,) as <arg2>:
+
+returns value depending on <arg1> and <arg2>.
+
+Liri uses:
+
+Node-Spotify-API,
+
+Request,
+
+Axios,
+
+Moment,
+
+DotEnv
+
+
+Screenshots:
+image1
+image2
+image3
+image4
---
+++ liri.js
@@ -0,0 +1,112 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const request = require("request");
+const keys = require('./keys.js');
+const axios = require("axios");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
+let action = process.argv[2];
+let title = process.argv.slice(3).join(" ");
+
+console.log(process.argv);
+
+function liriSwitch(action, title) {
+    switch (action) {
+        case "concert-this":
+            concertThis(title);
+            break;
+        case "movie-this":
+            movieThis(title);
+            break;
+        case "spotify-this-song":
+            spotifyThis(title);
+            break;
+        case "do-what-it-says":
+            doWhatItSays(title);
+            break;
+        default:
+            console.log("You can't do that!")
+            break;
+    }
+};
+
+function movieThis(search) {
+    console.log("movieThis")
+    if (name === "") {
+        name = "Mr. Nobody";
+    }
+    axios.get(`http://www.omdbapi.com/?t=${search}&y=&plot=short&apikey=trilogy`).then(
+        function (response) {
+            console.log("The movie's title: " + response.data.title);
+            console.log("The year of release: " + response.data.year);
+            console.log("The country of movie: " + response.data.country);
+            console.log("The movie's language: " + response.data.language);
+            console.log("The movie's actors: " + response.data.actors);
+            console.log("The movie's plot: " + response.data.plot);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+        }
+    );
+    fs.appendFile("log.txt", text, function (err) {
+        if (error) {
+            return console.log(error);
+        }
+    });
+
+    function concertThis(band) {
+        axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                console.log(response.data);
+                console.log(response.data[0].venue.name);
+                console.log("Date of event" + moment(response.data[0].datetime).format('MM/DD/YYYY'));
+                console.log(response.data[0].venue.city);
+            })
+        fs.appendFile("log.txt", text, function (err) {
+            if (error) {
+                return console.log(error);
+            }
+        });
+    };
+
+    function spotifyThis(song) {
+        if (name === "") {
+            name = "The Sign Ace of Base";
+        }
+
+        spotify.search({ type: 'track', query: song }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            function (response) {
+                console.log("Artist: " + response.data[0].artists.name);
+                console.log("Song: " + response.data[0].name);
+                console.log("Preview url: " + response.data.preview_url);
+                console.log("Album: " + response.data[0].album.name);
+            }
+            console.log(data);
+            fs.appendFile("log.txt", text, function (err) {
+                if (error) {
+                    return console.log(error);
+                }
+
+            })
+        });
+    };
+
+    function doWhatItSays() {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            action = data.split(",")[0].trim();
+            title = data.split(",")[1].trim();
+            liriSwitch(action);
+        })
+    };
+
+liriSwitch(action, title);
+
+
